e665dfa81b7b68d67684b5f2971c76795c1990d4
astroAi2 <ASTROAILLC@GMAIL.COM>
2025-07-13 09:20:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+console.log("ASTROAI Website Loaded");
+
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', () => {
+        alert("This section is coming soon!");
+    });
+});
